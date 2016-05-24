f1badcd652880ebfea847138de66b1de3b21f791
Jarrod Jones <Jaye1200@icloud.com>
2016-05-23 23:54:35 -0600

    Created a math program

---
+++ pyrubian_theorem/Readme.md
@@ -0,0 +1,1 @@
+> Just a quick program that utilizes the pythagorean theorem to figure out the length of a hypotenuse when the user supplies the length of the other 2 sides.
---
+++ pyrubian_theorem/pyrubian_theorem.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+puts 'Welcome to the pyrubian theorem program!'
+puts 'This program can use magical powers to reveal the hypotenuse of a triangle when you know the length of the other two sides.'
+puts '(This will not work for equilateral triangles due to the fact that all sides are the same length!)'
+
+print "First, what is the length of the first side? "
+begin
+  side_a = gets.chomp
+  side_a = Integer(side_a)
+rescue
+  print "Numbers only please! Try it again, what's the first side length? "
+  retry
+end
+
+print "Now tell me the length of the second side! "
+
+begin
+  side_b = gets.chomp
+  side_b = Integer(side_b)
+rescue
+  print "Numbers only silly! Try it again, what's the second side length? "
+  retry
+end
+
+hypotenuse = (side_a.to_i ** 2) + (side_b.to_i ** 2)
+
+puts 'I shall now work my magic...'
+
+print "The hypotenuse of your triangle is #{Math.sqrt(hypotenuse).round(2)}, voila!!"
